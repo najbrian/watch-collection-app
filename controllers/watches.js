afb41040349f86011745b117eaedcd1b1a19380f
najbrian <najbrian@gmail.com>
2024-08-03 11:30:33 -0700

    added watches index view, was going to start on the show page, but I noticed that when other users log into the site, they're seeing other user watches in their collection index page. currently working on that issue

--- controllers/watches.js
+++ controllers/watches.js
@@ -4,8 +4,48 @@ const router = express.Router();
 const User = require('../models/user.js');
 const Watch = require('../models/watch.js');
 
-router.get('/', (req, res) => {
-  res.render('watches/index.ejs')
+///users/:userId/watches
+
+router.get('/', async (req, res) => {
+  try {
+    const populatedWatches = await Watch.find({}).populate('owner')
+    currentUser = req.session.user
+    res.render('watches/index.ejs', { watches: populatedWatches, currentUser })
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+})
+
+router.get('/new', (req, res) => {
+  res.render('watches/new.ejs')
+})
+
+router.get('/:watchId', async (req, res) => {
+  try {
+    const watchData = await Watch.findById(req.params.watchId)
+    res.render('watches/show.ejs', { watch: watchData })
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    req.body.owner = req.session.user._id
+    if (req.body.isForSale === "on") {
+      req.body.isForSale = true;
+    } else {
+      req.body.isForSale = false;
+    }
+    await Watch.create(req.body)
+    res.redirect('/users/:userId/watches')
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+
 })
 
 module.exports = router
