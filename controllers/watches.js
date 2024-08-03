364adae9abf6c6fa34b7814b0c039817e9e21291
najbrian <najbrian@gmail.com>
2024-08-03 13:33:56 -0700

    added delete functionality

--- controllers/watches.js
+++ controllers/watches.js
@@ -8,7 +8,7 @@ const Watch = require('../models/watch.js');
 
 router.get('/', async (req, res) => {
   try {
-    const populatedWatches = await Watch.find({owner: req.session.user}).populate('owner')
+    const populatedWatches = await Watch.find({ owner: req.session.user }).populate('owner')
     res.render('watches/index.ejs', { watches: populatedWatches })
   } catch (error) {
     console.log(error)
@@ -33,7 +33,7 @@ router.get('/:watchId', async (req, res) => {
 router.get('/:watchId/edit', async (req, res) => {
   try {
     const editWatch = await Watch.findById(req.params.watchId).populate('owner')
-    res.render('watches/edit.ejs', {watch: editWatch})
+    res.render('watches/edit.ejs', { watch: editWatch })
   } catch (error) {
     console.log(error)
     res.redirect('/')
@@ -49,7 +49,7 @@ router.post('/', async (req, res) => {
       req.body.isForSale = false;
     }
     await Watch.create(req.body)
-    res.redirect('/users/:userId/watches')
+    res.redirect(`/users/${req.session.user._id}/watches`)
   } catch (error) {
     console.log(error)
     res.redirect('/')
@@ -57,22 +57,37 @@ router.post('/', async (req, res) => {
 
 })
 
-router.put('/:watchId', async (req, res) => {
-try {
-  req.body.owner = req.session.user._id
-  if(req.body.isForSale === 'on') {
-    req.body.isForSale = true
+router.delete('/:watchId', async (req, res) => {
+  try {
+    const currentWatch = await Watch.findById(req.params.watchId)
+    if(currentWatch.owner.equals(req.session.user._id)) {
+    await currentWatch.deleteOne()
+    res.redirect(`/users/${req.session.user._id}/watches`)
   } else {
-    req.body.isForSale = false
+    res.redirect('/')
   }
-  await Watch.findByIdAndUpdate(req.params.watchId, req.body)
-  res.redirect(`/users/${req.session.user._id}/watches/${req.params.watchId}`)
-
-}catch (error) {
-  console.log(error)
-  res.redirect('/')
-}
+  
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
 })
 
+router.put('/:watchId', async (req, res) => {
+  try {
+    req.body.owner = req.session.user._id
+    if (req.body.isForSale === 'on') {
+      req.body.isForSale = true
+    } else {
+      req.body.isForSale = false
+    }
+    await Watch.findByIdAndUpdate(req.params.watchId, req.body)
+    res.redirect(`/users/${req.session.user._id}/watches/${req.params.watchId}`)
+
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+})
 
 module.exports = router
