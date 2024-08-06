1b38772812962c9f055bcd41f5988344cec64829
najbrian <najbrian@gmail.com>
2024-08-05 22:44:55 -0700

    Added favorite functionality, getting issues with comment functionality - saving what I have for now

--- controllers/collections.js
+++ controllers/collections.js
@@ -10,15 +10,61 @@ router.get('/', async (req, res) => {
   res.render('collections/index.ejs', { otherUsers })
 })
 
-router.get('/:userId', async (req, res) => {
-  const userCollection = await Watch.find({ owner: req.params.userId }).populate('owner')
-  const userDetails = await User.findById(req.params.userId)
+router.get('/:ownerId', async (req, res) => {
+  const userCollection = await Watch.find({ owner: req.params.ownerId }).populate('owner')
+  const userDetails = await User.findById(req.params.ownerId)
   res.render('collections/other-users.ejs', { userCollection, userDetails })
 })
 
-router.get('/:userId/:watchId', async (req, res) => {
+router.get('/:ownerId/:watchId', async (req, res) => {
+  try {
   const userWatch = await Watch.findById(req.params.watchId).populate('owner')
-res.render('collections/show.ejs', {userWatch})
+  const userHasFavorited = userWatch.favoritedByUsers.some((user) => user.equals(req.session.user._id))
+  const comments = userWatch.commentsByUsers.find(req.params.watchId)
+  res.render('collections/show.ejs', { userWatch, userHasFavorited, comments })
+  }catch(error) {
+    console.log(error)
+    res.redirect('/')
+  }
+})
+
+router.post('/:ownerId/:watchId/comments', async (req,res) => {
+  try{
+    req.body.owner = req.session.user._id
+    const currentWatch = await Watch.findById(req.params.watchId)
+    currentWatch.commentsByUsers.push(req.body)
+  
+    await currentWatch.save()
+    res.render('collections/show.ejs', {currentWatch})
+
+  } catch(error) {
+    console.log(error)
+    res.redirect('/')
+  }
+})
+
+router.post('/:ownerId/:watchId/favorited-by/:userId', async (req, res) => {
+  try {
+    await Watch.findByIdAndUpdate(req.params.watchId, {
+      $push: { favoritedByUsers: req.params.userId },
+    })
+    res.redirect(`/collections/${req.params.ownerId}/${req.params.watchId}`)
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+})
+
+router.delete('/:ownerId/:watchId/favorited-by/:userId', async (req, res) => {
+  try {
+    await Watch.findByIdAndUpdate(req.params.watchId, {
+      $pull: { favoritedByUsers: req.params.userId },
+    })
+    res.redirect(`/collections/${req.params.ownerId}/${req.params.watchId}`)
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
 })
 
 module.exports = router
